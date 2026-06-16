0ed32f579a835ec297c3764d32c63d5a5430434e
Rodolfo880 <rodolforodriguezculqui@gmail.com>
2026-06-16 14:05:53 -0500

    Agregamos clase Venta, los atributos, Getter and Setter y metodos.

---
+++ src/proyectofinal_tpoo/Venta.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectofinal_tpoo;
+
+import java.util.Date;
+import java.sql.Time;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Venta {
+     // Atributos
+    private int idVenta;
+    private Date fecha;
+    private Time hora;
+    private double subtotal;
+    private double igv;
+    private double total;
+    
+    // Composición con DetalleVenta
+    private ArrayList<DetalleVenta> detallesVenta;
+
+    // Constructor vacío
+    public Venta() {
+        detallesVenta = new ArrayList<>();
+    }
+    
+    // Constructor con parámetros
+    public Venta(int idVenta, Date fecha, Time hora, double subtotal,
+                 double igv, double total, ArrayList<DetalleVenta> detallesVenta) {
+        this.idVenta = idVenta;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.subtotal = subtotal;
+        this.igv = igv;
+        this.total = total;
+        this.detallesVenta = detallesVenta;
+    }
+
+    // Getters y Setters
+
+    public int getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(int idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Time getHora() {
+        return hora;
+    }
+
+    public void setHora(Time hora) {
+        this.hora = hora;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public double getIgv() {
+        return igv;
+    }
+
+    public void setIgv(double igv) {
+        this.igv = igv;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public ArrayList<DetalleVenta> getDetallesVenta() {
+        return detallesVenta;
+    }
+
+    public void setDetallesVenta(ArrayList<DetalleVenta> detallesVenta) {
+        this.detallesVenta = detallesVenta;
+    }
+
+    // Métodos
+    public void registrarVenta() {
+    }
+
+    public double calcularTotal() {
+        return 0;
+    }
+    
+    public void imprimirVenta() {
+    }
+}
