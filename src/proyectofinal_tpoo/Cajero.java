ba976b4b759da2fa4acee7ee818040a42d06f454
Rodolfo880 <rodolforodriguezculqui@gmail.com>
2026-06-16 12:35:52 -0500

    Agregamos clase Cajero, los atributos, Getter and Setter y metodos.

---
+++ src/proyectofinal_tpoo/Cajero.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectofinal_tpoo;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Cajero {
+    private String turno;
+
+    public Cajero() {
+    }
+    
+     // Constructor con parámetro
+    public Cajero(String turno) {
+        this.turno = turno;
+    }
+    
+     // Getter y Setter
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+
+    // Métodos
+    public void registrarVenta() {
+    }
+
+    public void consultarProducto() {
+    }
+}
